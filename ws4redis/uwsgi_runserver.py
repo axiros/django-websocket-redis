680e97cfb954c07d611f0861def457a33023d53c
Vaysberg <vladislav.vaysberg@axiros.com>
2018-09-25 11:25:02 +0200

    little uwsgi error fix

--- ws4redis/uwsgi_runserver.py
+++ ws4redis/uwsgi_runserver.py
@@ -8,7 +8,7 @@ from ws4redis.wsgi_server import WebsocketWSGIServer
 class uWSGIWebsocket(object):
     def __init__(self):
         self._closed = False
-        self._filter = {}
+        self.filter = {}
 
     def get_file_descriptor(self):
         """Return the file descriptor for the given websocket"""
@@ -51,7 +51,7 @@ class uWSGIWebsocket(object):
 class uWSGIWebsocketServer(WebsocketWSGIServer):
     def upgrade_websocket(self, environ, start_response):
         uwsgi.websocket_handshake(environ['HTTP_SEC_WEBSOCKET_KEY'], environ.get('HTTP_ORIGIN', ''))
-        return uWSGIWebsocket(environ)
+        return uWSGIWebsocket()
 
     def select(self, rlist, wlist, xlist, timeout=None):
         return gevent.select.select(rlist, wlist, xlist, timeout)
